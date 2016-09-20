6f8a084f11bd8ff69af6977cc7a6e79d7f59867f
Khoa Bui <khoabui@Khoas-MacBook-Pro.local>
2016-09-20 22:53:16 +0300

    update post controller

--- server/routes/posts/index.js
+++ server/routes/posts/index.js
@@ -1,1 +1,14 @@
+const { Router } = require('express');
+const { index, showBriefs, update, create, remove, getOne, getCategoryPost} = require('./post.controller');
 
+const router = Router() ;
+
+router.get('/',  showBriefs);
+router.get('/brief', showBriefs);
+router.get('/:postId', getOne);
+router.post('/', create);
+router.put('/:postId', update);
+router.delete('/:postId', remove);
+router.get('/category/:categorySlug', getCategoryPost);
+
+module.exports = router;
