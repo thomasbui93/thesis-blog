03e10e2510497563e2cabdcfd8b2262207d892a7
Khoa Bui <khoabui@Khoas-MacBook-Pro.local>
2016-09-20 22:55:44 +0300

    add authentication middleware

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -1,1 +1,22 @@
+const {checkAuth} = require('../user/user.controller');
+
+module.exports = (req, res, next)=>{
+    let token = req.body.token;
+    if(token===undefined || token === null){
+        token = req.query.token
+    }
+    if(req.method !=='GET'){
+        checkAuth(token).then((response)=>{
+            next()
+        }, (error)=>{
+            console.log(error);
+            res.status(403).json({
+                message: "not authorized"
+            });
+        });
+    }else {
+        next();
+    }
+}
+
 
